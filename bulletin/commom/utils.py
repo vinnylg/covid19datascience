fcfbb457af462de42a073d66fb4114c61d398b59
Vinicius de Lima Gonçalves <viniciusslimaa@outlook.com>
2020-11-18 08:45:59 -0300

    processo diario improve

---
+++ bulletin/commom/utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from os.path import dirname, join
+from bulletin import __file__ as __root__
+
+municipios = pd.read_csv(join(dirname(__root__),'resources','csv','municipios.csv'))
+paises = pd.read_csv(join(dirname(__root__),'resources','csv','paises.csv'))
+regionais = pd.read_csv(join(dirname(__root__),'resources','csv','regionais.csv'))
+termos = pd.read_csv(join(dirname(__root__),'resources','csv','termos.csv'))
