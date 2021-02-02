3e64bb8d244ccbcc04623ec1717ca9ac8397c0db
Vinicius de Lima Gonçalves <viniciusslimaa@outlook.com>
2021-02-02 11:23:53 -0300

    fix year date relatorio

--- bulletin/commom/static.py
+++ bulletin/commom/static.py
@@ -8,4 +8,4 @@ regionais = pd.read_csv(join(dirname(__root__),'resources','csv','regionais.csv'
 termos = pd.read_csv(join(dirname(__root__),'resources','csv','termos.csv'))
 se = pd.read_csv(join(dirname(__root__),'resources','csv','se.csv'), parse_dates=['inicio','fim'])
 
-meses = ['jan/2021','fev','mar','abril','mai','jun','jul','ago','set','out','nov','dez']
+meses = ['jan/2021','fev/2021','mar/2020','abril/2020','mai/2020','jun/2020','jul/2020','ago/2020','set/2020','out/2020','nov/2020','dez/2020']
