ad056d24870304a8911e14ac1aa4e397ba8c5f84
Vinicius de Lima Gonçalves <viniciusslimaa@outlook.com>
2021-02-10 08:44:29 -0300

    try improve merge notifica

--- bulletin/commom/utils.py
+++ bulletin/commom/utils.py
@@ -55,52 +55,50 @@ def get_better_notifica(df):
         _, row = serie
 
         if row['idade'] != -99:
-            scores[i]+=50
+            scores[i]+=1
 
         if row['nome_mae']:
-            scores[i]+=50
+            scores[i]+=1
 
         if row['cpf']:
-            scores[i]+=50
+            scores[i]+=1
 
-        if not row['nome_mae'] and not row['cpf'] and row['data_nascimento'] == pd.NaT:
-            scores-=500
+        # if not row['nome_mae'] and not row['cpf'] and row['data_nascimento'] == pd.NaT:
+        #     scores-=500
 
         if row['cod_classificacao_final'] == 2:
-            scores[i]+=100
+            scores[i]+=10
 
         if row['cod_criterio_classificacao'] == 1:
-            scores[i]+=10
+            scores[i]+=5
 
         if row['cod_evolucao'] == 2:
             if row['data_cura_obito'] != pd.NaT:
-                scores[i]+=1000
-            else:
-                scores[i]-=100
+                scores[i]+=10
 
         if row['cod_metodo'] == 1:
-            scores[i]+=10
+            scores[i]+=1
 
         if row['cod_status_notificacao'] in [1, 2]:
-            scores[i]+=10
+            scores[i]+=1
 
         if row['excluir_ficha'] == 2:
-            scores[i]+=10
+            scores[i]+=1
 
         if row['cod_origem'] == 1:
-            scores[i]+=10
+            scores[i]+=1
 
         if row['data_1o_sintomas'] != pd.NaT:
-            scores[i]+=50
+            scores[i]+=1
 
         if row['data_coleta'] != pd.NaT:
-            scores[i]+=40
+            scores[i]+=1
 
         if row['data_recebimento'] != pd.NaT:
-            scores[i]+=30
+            scores[i]+=1
 
         if row['data_liberacao'] != pd.NaT:
-            scores[i]+=20
+            scores[i]+=1
 
     i = np.argmax(scores)
     return df.iloc[i].name
